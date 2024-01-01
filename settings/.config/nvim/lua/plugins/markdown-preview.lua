4dd52d60acb7e3dcc759749fae0bedac08b513a3
Lucas <lluc23@hotmail.com>
2024-01-02 00:09:48 +0100

    2024_01_02T00_09_35

--- settings/.config/nvim/lua/plugins/markdown-preview.lua
+++ settings/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,12 @@
 return {
-    "lucas-montes/markdown-preview.nvim",
-    lazy = true,
+    "iamcco/markdown-preview.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
     keys = {
         {
 			"<leader>md",
@@ -13,5 +19,31 @@ return {
 			desc = "Stop visualization",
 		  },
     },
-	run = function() vim.fn["mkdp#util#install"]() end,
-}
+    ft = { "markdown" },
+    config = function()
+      local job = require("plenary.job")
+      local install_path = vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim/app"
+      local cmd = "bash"
+
+      if vim.fn.has("win64") == 1 then
+        cmd = "pwsh"
+      end
+
+      job
+        :new({
+          command = cmd,
+          args = { "-c", "npm install && git restore ." },
+          cwd = install_path,
+          on_exit = function()
+            print("Finished installing markdown-preview.nvim")
+          end,
+          on_stderr = function(_, data)
+            print(data)
+          end,
+        })
+        :start()
+
+      -- Options
+      vim.g.mkdp_auto_close = 0
+    end,
+  }
